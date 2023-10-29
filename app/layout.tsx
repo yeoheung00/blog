50c9bda3d22374465a248a8e04b10b1ea95a0ac4
yeoheung00 <yeoheung27@gmail.com>
2023-10-29 09:59:17 +0900

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,27 @@ export default function docLayout({ children }: { children: React.ReactNode }) {
       <body className={inter.className}>
         <script
           dangerouslySetInnerHTML={{
-            __html: `(${String(getThemeFromLocalStorage)})()`,
+            __html: `
+              const theme = window.localStorage.getItem('theme');
+              const isThemeExiest = typeof theme === 'string';
+
+              const preference = window.matchMedia('(prefers-color-scheme: dark)');
+              const isPreferenceExiest = typeof preference.matches === 'boolean';
+
+              let theme_state = 'light';
+
+              if (isThemeExiest) {
+                theme_state = theme;
+              } else if (isPreferenceExiest) {
+                theme_state = preference.matches ? 'dark' : 'light';
+              }
+
+              const doc = document.body;
+
+              doc.style.setProperty('--initial-color-mode', theme_state);
+
+              if (theme_state === 'dark') doc.setAttribute('data-theme', 'dark');
+            `,
           }}
         />
         <Header />
