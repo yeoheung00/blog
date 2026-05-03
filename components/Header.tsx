ef6a02f43edd63ffa8725d984c662b44c1e812d7
yeoheung00 <yeoheung27@gmail.com>
2026-05-03 13:01:16 +0900

    remake start

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
         <header className="sticky top-0 z-50 w-full bg-background">
             <div className="max-w-5xl mx-auto px-4 h-10 flex items-center justify-between">
                 <Link href="/" className="text-2xl font-bold tracking-tighter pt-1">
-                    <span className="text-primary">M</span>in<span className="text-primary">K</span> Studio
+                    <span className="text-primary">M</span>in<span className="text-primary">K</span> Studio(remakeing...)
                 </Link>
                 <div className="flex items-center gap-3">
                     <Link key="/about" href="/about" className="text-md font-bold pt-1">
